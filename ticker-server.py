032dee42c27946799f325a108948f4cf3d397fd7
jason <jesusislord3@protonmail.com>
2017-06-12 21:58:53 +0100

    Implementing New Live Strream Interface - As opposed to total hacks.

--- ticker-server.py
+++ ticker-server.py
@@ -5,6 +5,9 @@ import math
 # https://github.com/femtotrader/pandas_datareaders_unofficial/commit/b3008b1eb9e2fd1cd86efd6561296fcb5adf0bd8 some copy and paste.
 import time
 import threading
+from bokeh.core.properties import ColumnData
+from bokeh.models import ColumnDataSource, Slider, Select
+from bokeh.plotting import curdoc, figure
 
 auth_response = truefx.login('jesusislord3','Anglocat777',['EUR/USD'])
 
@@ -18,6 +21,7 @@ lows = []
 timestamps = []
 response = ''
 
+
 #graph settings
 tickrate = 0.25 # official tick rate
 duration_to_plot = 3600 # in seconds
@@ -26,17 +30,6 @@ bootstrap = math.ceil(duration_to_plot / tickrate) # round up to give a rough es
 # https://www.continuum.io/content/painless-streaming-plots-bokeh
 
 # from this example - http://stackoverflow.com/a/18793416
-
-
-
-"""
-time.sleep(10)
-thread_stopper.set()
-print('length of labels:',len(timestamps),'length of rates',len(closes))
-
-time.sleep(2)
-print(opens,closes,highs,lows,timestamps)
-"""
 
 ###  BEGIN GRAPH PLOTTING
 import numpy as np
@@ -60,8 +53,6 @@ p = figure(width=800, height=350, x_axis_type="datetime")
 
 # p.circle(aapl_dates, aapl, size=4, color='darkgrey', alpha=0.2, legend='close')
 
-closeline = p.line('y', 'x', color='black', legend='Close')
-openline =  p.line('y', 'x', color='blue', legend='Open')
 # p.line(aapl_dates, np.array(highs), color='green', legend='Highs') - values lack enough precision to be usualble
 # p.line(aapl_dates, np.array(lows), color='red', legend='Lows') - values make no sense for now.
 
@@ -78,45 +69,56 @@ p.ygrid.band_fill_alpha = 0.1
 
 # prep the data interfaces
 
+"""
 opends = openline.data_source
 closeds = closeline.data_source
+"""
 
 # implement the server end. - http://bokeh.pydata.org/en/latest/docs/user_guide/server.html#userguide-server-applications
-
+# latest live ticker example - https://github.com/bokeh/bokeh/blob/master/examples/app/ohlc/main.py
+# extra hel;p on new feature - https://stackoverflow.com/questions/24800071/timeseries-streaming-in-bokeh
 # start polling data
 
+source = ColumnDataSource(truefx.poll_one_pair(auth_response).update({'actual_figure':actual_figure(truefx.poll_one_pair(auth_response)['bigbid'],truefx.poll_one_pair(auth_response)['bidpips'])})) # try this during tickerr hours.
 
+closeline = p.line(y='', x='millisecond-timestamp', color='black', legend='Close',source=source )
+openline = p.line(y='open', x='millisecond-timestamp', color='blue', legend='Open', source=source )
 
 def data_listener(stop_interupt_signal:threading.Event):
-    while (not stop_interupt_signal.is_set()):
+    loop_running = not stop_interupt_signal.isSet()
+    print('Data Listener Started')
+    timestamps = []
+    print('Loop Running', loop_running)
+    """
+    while loop_running:
         time.sleep(tickrate)
         current_poll = truefx.poll_one_pair(auth_response)
+        print(current_poll)
         print('Tick') if current_poll else print('Sorry Kids - Markets Closed.')
-        if not timestamps: # if first entry
-            timestamps = current_poll['millisecond-timestamp']
-            print('Current Poll is', current_poll['response'])
-            opens = float(current_poll['pollopen'])
-            closes = actual_figure(current_poll['bigbid'], current_poll['bidpips'])
-            highs = float(current_poll['high'])
-            lows = float(current_poll['low'])
-        if timestamps[-1] != current_poll['millisecond-timestamp']:
+        if not timestamps or timestamps[-1] != current_poll['millisecond-timestamp']:
             print('Current Poll is',current_poll['response'])
-            opens = float(current_poll['pollopen'])
-            closes = actual_figure(current_poll['bigbid'],current_poll['bidpips'])
+            opens = opends.data['y'] + [float(current_poll['pollopen'])]
+            closes = closeds.data['y'] + [actual_figure(current_poll['bigbid'],current_poll['bidpips'])]
             highs = float(current_poll['high'])
             lows = float(current_poll['low'])
-            timestamps = current_poll['millisecond-timestamp']
+            timestamps = opends.data['x'] + [current_poll['millisecond-timestamp']] # opends/closeds - could use either really.
+
         else:
-            pass
-        # pump to dataset
-        opends.data  = {'x':timestamps,'y':opens,'color':'black','legend':'Close'}
-        closeds.data  = {'x':timestamps,'y':closes,'color':'blue','legend':'Open'}
+            print("skipped loop",current_poll)
+
+        # pump one tick to dataset
+        opends.data['y'],opends.data['x'] = (opens,timestamps)
+        closeds.data['y'],closeds.data['x'] = (closes,timestamps)
+        p.update()
+    """
 
 thread_stopper = threading.Event()
-thread = threading.Thread(target=data_listener,args=(thread_stopper,))
-thread.daemon = True # do not be afraid christians! they dont exist (see galatians-12.) fear  is of the devil anyways :D
-thread.start()
-print('Data Listener Started - Showing Graph')
+"""
+    thread = threading.Thread(target=data_listener,args=(thread_stopper,))
+    thread.daemon = True # do not be afraid christians! they dont exist (see galatians-12.) fear  is of the devil anyways :D
+    thread.start()
+"""
+print('Showing Graph')
 
 show(p)
-
+data_listener(thread_stopper)
